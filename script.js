03eace368e31991191e3663b1721b4ed16fa82b1
Iago Vieira Vilela <d2023012249@unifei.edu.br>
2025-09-12 16:26:11 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
+// Função para a seção Cabelo
 function openHair(evt, hairTypeName) {
-    // Declara todas as variáveis
     var i, haircontent, tablinks;
-
-    // Pega todos os elementos com a classe="hair-content" e os esconde
     haircontent = document.getElementsByClassName("hair-content");
     for (i = 0; i < haircontent.length; i++) {
         haircontent[i].style.display = "none";
     }
-
-    // Pega todos os elementos com a classe="tablink" e remove a classe "active"
     tablinks = document.getElementsByClassName("tablink");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
-    // Mostra a aba atual e adiciona uma classe "active" ao botão que abriu a aba
     document.getElementById(hairTypeName).style.display = "block";
     evt.currentTarget.className += " active";
 }
 
-// Abre a primeira aba por padrão
+// NOVA Função para a seção Maquiagem
+function openMakeup(evt, makeupTypeName) {
+    var i, makeupcontent, tablinks;
+    makeupcontent = document.getElementsByClassName("makeup-content");
+    for (i = 0; i < makeupcontent.length; i++) {
+        makeupcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink-makeup");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(makeupTypeName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
+// Garante que a primeira aba de cada seção seja aberta por padrão
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementsByClassName("tablink")[0].click();
+    // Para a página de Cabelo
+    if (document.querySelector(".tablink")) {
+        document.getElementsByClassName("tablink")[0].click();
+    }
+    // Para a página de Maquiagem
+    if (document.querySelector(".tablink-makeup")) {
+        document.getElementsByClassName("tablink-makeup")[0].click();
+    }
 });
